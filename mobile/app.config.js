0a91ca3f1aaf9917f4bbfefbca3a9b5130780206
eremef <34741103+eremef@users.noreply.github.com>
2026-03-12 03:15:16 +0100

    chore: release v1.8.4-alpha.1

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -9,7 +9,7 @@ module.exports = {
   expo: {
     name: 'Beta Player',
     slug: 'beta-app',
-    version: '1.8.3',
+    version: '1.8.4-alpha.1',
     orientation: 'portrait',
     icon: './assets/icon.png',
     scheme: 'beta-app',
