3b933bb7d3a7c306dd8dca87b3de92a803f37286
eremef <34741103+eremef@users.noreply.github.com>
2026-07-21 18:00:31 +0200

    chore: release v1.8.20-beta

--- mobile/app.config.js
+++ mobile/app.config.js
@@ -2,7 +2,7 @@ module.exports = {
   expo: {
     name: 'Beta Player',
     slug: 'bandcamp-remote',
-    version: '1.8.19-beta',
+    version: '1.8.20-beta',
     orientation: 'portrait',
     icon: './assets/icon.png',
     scheme: 'beta-app',
